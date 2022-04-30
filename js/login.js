48f974e939d9eefd0bd3235d4d099c9afba08aef
tozturk18 <tunaozturk2001@hotmail.com>
2022-04-30 18:18:15 -0400

    finally the splash page works!

--- js/login.js
+++ js/login.js
@@ -27,17 +27,22 @@ function delCookie (name) {
     document.cookie = name;
 }
 
-function logIn() {
-    $("#loginEnv").css("display", "grid");
-    $("#loginEnv").hide();
-    $("#loginEnv").fadeIn();
+var elementClicked;
+var logged;
 
-    $("#loginSubmit").click(completeLogin)
+function logIn() {
+
+    if (!logged) {
+        $("#loginEnv").css("display", "grid");
+        $("#loginEnv").hide();
+        $("#loginEnv").fadeIn();
+    } else {
+        logOut();
+    }
+    
 }
 
-var elementClicked;
-
-function logOut(){
+function closeLogIn(){
 
     if(!elementClicked) {
         $("#loginEnv").fadeOut();
@@ -51,10 +56,112 @@ function logOut(){
 }
 
 function completeLogin() {
-    let username = $("username").val();
-    let password = $("password").val();
 
-    console.log("username: " + username +"\npassowrd: " + password);
+    let username = $("#username").val();
+    let password = $("#password").val();
+
+    console.log("username: " + username + "\npassowrd: " + password);
+
+    if (username != "" && password != "" ) {
+
+        if ( ( username == "Joe" && password == "joe123" ) || ( username == "Jane" && password == "jane999" ) ) {
+            logged = true;
+
+            $('svg').remove();
+            var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+            $("#loginIcon").append( svg );
+            $('svg').attr({height:50, width: 50});
+
+            var circle1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+            var circle2 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+            var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+            $('svg').append(circle1);
+            $('svg').append(circle2);
+            $('svg').append(path);
+
+            var color = "";
+
+            if (getCookie("color") == "") {
+                var rand = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' ];
+                color = '#' + rand[Math.ceil(Math.random() * 15)] + rand[Math.ceil(Math.random() * 15)] + rand[Math.ceil(Math.random() * 15)] + rand[Math.ceil(Math.random() * 15)] + rand[Math.ceil(Math.random() * 15)] + rand[Math.ceil(Math.random() * 15)];
+
+                setCookie("color", color);
+
+            } else {
+                color = getCookie("color");
+            }
+
+            $(circle1).attr({cx:25, cy:25, r:20, stroke:"white", strokeWidth:1, fill:color});
+            $(circle2).attr({cx:25, cy:20, r:7.5, stroke:"white", strokeWidth:1, fill:"lightgray"});
+            $(path).attr({d:"M 16 42.5 q 9 -22.5 18 0", stroke:"white", strokeWidth:1, fill:"lightgray"});
+
+            closeLogIn();
+
+            setCookie("username", username);
+            
+
+            $("#iconText").text("logout   " + username);
+
+        } else {
+            alert("username and password cannot be matched!");
+        }
+    } else {
+        alert("Username and Password must be entered.");
+    }
+}
+
+function logOut() {
+
+    logged = false;
+
+    $('svg').remove();
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    $("#loginIcon").append( svg );
+    $('svg').attr({height:50, width: 50});
+
+    var circle1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    var circle2 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+    $('svg').append(circle1);
+    $('svg').append(circle2);
+    $('svg').append(path);
+
+    $(circle1).attr({cx:25, cy:25, r:20, stroke:"white", strokeWidth:1, fill:"gray"});
+    $(circle2).attr({cx:25, cy:20, r:7.5, stroke:"white", strokeWidth:1, fill:"lightgray"});
+    $(path).attr({d:"M 16 42.5 q 9 -22.5 18 0", stroke:"white", strokeWidth:1, fill:"lightgray"});
+
+    $("#iconText").text("login");
+
+    delCookie("username");
+    delCookie("color");
+
+}
+
+function buttons() {
+
+    if (logged) {
+        $("#mail").click(pageNotExist);
+        $("#fotoFan")
+        $("#drive").click(pageNotExist);
+        $("#GPS").click(pageNotExist);
+        $("#games").click(pageNotExist);
+        $("#food").click(pageNotExist);
+        $("#finance").click(pageNotExist);
+        $("#browser").click(pageNotExist);
+        $(".searchBtn").click(pageNotExist);
+    } else {
+        logIn();
+    }
+    
+
+    
+
+}
+
+function pageNotExist() {
+    window.location.href = "./404.html";
 }
 
 function loader() {
@@ -64,8 +171,11 @@ function loader() {
     });
     
     $("#loginIcon").click(logIn);
-    $("#loginEnv").click(logOut);
+    $("#loginEnv").click(closeLogIn);
+
     $("#loginSubmit").click(completeLogin);
+
+    buttons();
 }
 
 window.onload = loader;
