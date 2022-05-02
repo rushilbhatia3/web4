0923596e572c717ee47448a617627218ab63b56e
rushilbhatia3 <33175847+rushilbhatia3@users.noreply.github.com>
2022-05-02 02:20:55 -0400

    random

--- fotofan.php
+++ fotofan.php
@@ -3,9 +3,11 @@
 
 <head>
     <META HTTP-EQUIV="Refresh" CONTENT="0; URL=http://cs.wheatoncollege.edu/~rbhatia/web/wRB4/fotofan.php">
+    </META>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Yippee! - FotoFan</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -13,6 +15,7 @@
     <link rel="stylesheet" href="css/carousel.css">
 
     <script src="js/fotofan.js"></script>
+
 </head>
 
 <body>
--- login.php
+++ login.php
@@ -1,21 +1,17 @@
 <?php
     $password= $_POST["psw"];
-    $conn = new mysqli("localhost","rbhatia", $password, "rbhatia");
+    echo "<p>psw:</p>" +$_POST["psw"]
+    $conn = new mysqli("localhost","rbhatia", "5926", "rbhatia");
     if($conn->connect_error){
         echo"<p>failed to conect</p>";
         
         die($conn->error);
     }
     else{
-        $myQuery = "select * from eastern";
-        $result= $conn->query($myQuery);
-        if(!$result){
-            echo"<p>empty db</p>";
-            die($conn->error);
-        }
-        else{
-            header('Location: http://cs.wheatoncollege.edu/~rbhatia/web/wRB4/fotofan.php');
-exit;
+        header("Location:/fotofan.php");
+            //die();
+        exit();
+            //echo file_get_contents()
 
     }
     ?>
