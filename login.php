30c7970156358fab66ec73de609c78a261dc677e
rushilbhatia3 <33175847+rushilbhatia3@users.noreply.github.com>
2022-05-02 01:11:09 -0400

    login fix attempt 1

--- login.php
+++ login.php
@@ -14,20 +14,8 @@
             die($conn->error);
         }
         else{
-            for($i=0;$i<$result->num_rows;$i++){
-                $result->data_seek($i);
-                $row=$result->fetch_array(MYSQLI_ASSOC);
-                echo '<div id="right"><p>name: ', $row["name"], "</p>";
-                echo "<img src= ", $row["fname"] , "></div>";
-                   
-                
-            }
-            //$obj = $result->fetch_object();
-            
-            //echo"<p>name: $obj->name</p>";
-            //echo"<p>elevation: $obj->elevation</p>";
-            //echo"<p>avg snow: $obj->avgSnow</p>";
-            //echo"<p>id: $obj->idnum</p>";
-        }
+            header('Location: yippee.html');
+exit;
+
     }
     ?>
